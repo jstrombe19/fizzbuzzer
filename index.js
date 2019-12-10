23f8a485f04345fa4d919be8f3c441fe75983a72
jstrombe19 <jaredstromberg@gmail.com>
2019-12-10 12:42:00 -0700

    Push of completed solution to fizzbuzz interview question with only a single iteration for either solution. The solution includes a form which will produce the correct result according to the standard fizzbuzz question of being divisible by 3 or 5 or both upon submission. The result will be appended to the screen. The results will not persist.

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-import React from 'react';
+document.addEventListener('DOMContentLoaded', () => {
+  const body = document.querySelector('body');
+  const header = document.createElement('h1');
+  const numericInputForm = document.createElement('form');
+  const numericInputField = document.createElement('input');
+  const fizzBuzzButton = document.createElement('button');
 
-export default class Index extends React.Component {
-  constructor(){
-    super()
+  header.innerHTML = 'Welcome to FizzBuzzer!';
+  body.appendChild(header);
+
+  function fizzBuzz(number) {
+    returnPhrase = '';
+    if (number % 3 === 0) {
+      returnPhrase += 'fizz';
+    }
+    if (number % 5 === 0) {
+      returnPhrase += 'buzz';
+    }
+    return returnPhrase;
   }
 
-  render() {
-    return(
-      
-    )
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('FIZZBUZZ!!');
+    const number = event.target[0].value;
+    const result = document.createElement('h3');
+    result.innerHTML = fizzBuzz(number);
+    body.appendChild(result);
   }
-}
+
+  numericInputField.setAttribute('type', 'text');
+  numericInputField.setAttribute('id', 'numeric-input-field');
+  fizzBuzzButton.setAttribute('type', 'submit');
+  fizzBuzzButton.setAttribute('id', 'submit');
+  fizzBuzzButton.innerText = 'FizzBuzz!';
+  numericInputForm.append(numericInputField, fizzBuzzButton);
+  numericInputForm.addEventListener('submit', handleSubmit);
+  body.appendChild(numericInputForm);
+});
